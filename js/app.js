e787f99cd0894de216524f39b1ccef201a125e76
Ricky Crawford <anguyen@lmanguyen020636.attlocal.net>
2021-01-26 20:03:47 -0800

    writing first function

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+let gameBoard = document.getElementById("gameBoard")
 
+let player1 = []
+let player2 = []
 
+let turns = 0
+
+gameBoard.addEventListener('click', boxClick) 
+
+function boxClick(e)
