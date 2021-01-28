53a7a455154dec3ece3094cfb0a80fb268f9e0a9
Ricky Crawford <anguyen@lmanguyen020636.local>
2021-01-28 09:18:11 -0800

    js finished game set up/ working on winner

--- js/app.js
+++ js/app.js
@@ -3,23 +3,62 @@ const gameBoard = document.getElementById("gameBoard")
 
 
 //let winners = []
-//let player1Selection = 'blackDisc'
-//let player2Selection = 'redDisc'
- let currentPlayer = "player1"
+let player1Selection = []
+let player2Selection = []
+let currentPlayer = "player1"
 // let points1 = 0
 // let points = 0
 
 // event.target is what is going to be clicked(pokersquare game)
-gameBoard.addEventListener('click', changeTurn) 
+//gameBoard.addEventListener('click', changeTurn) 
 
-function changeTurn(){
-    if (currentPlayer === "player1"){
+function changeTurn(e) {
+    //console.log(e)
+    let id = e.target.id
+    //console.log(id)
+    e.target.removeEventListener('click',changeTurn)
+    if (currentPlayer === "player1") {
+        player1Selection.push(id)
+        console.log(player1Selection)
+        e.target.style.backgroundColor = "black"
         currentPlayer = "player2"
-    } else if (currentPlayer === "player2") { 
+    } else if (currentPlayer === "player2") {        
+        player2Selection.push(id)
+        console.log(player2Selection)
+        e.target.style.backgroundColor = "red"
         currentPlayer = "player1"
     }
-    console.log(currentPlayer)
+    
+    // console.log(currentPlayer)
+}
+
+function addEventListeners() {
+    let squares = document.getElementsByClassName("grid")
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', changeTurn)
     }
+}
+function removeEventListeners() {
+    let squares = document.getElementsByClassName("grid")
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].removeEventListener('click', changeTurn)
+    }
+}
+addEventListeners()
+
+function checkForWins(square, player){
+    let possibleWins = [
+        ["row-1","row-2","row-3"], 
+        ["row-2","row-3","row-4"],
+        ["row-4","row-5","row-6"],
+        ["row-3","row-4","row-5"],
+    ]
+    
+}
+
+
+
+// event.target is what is going to be clicked(pokersquare game)
 
 
 
